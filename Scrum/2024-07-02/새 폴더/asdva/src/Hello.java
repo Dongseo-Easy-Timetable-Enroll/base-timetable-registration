825d5c20f0e04a8dbd498dc57edfbc8b0007a578
hyunjin4270 <hyunn4270@gmail.com>
2024-07-02 23:51:56 +0900

    write daliyscrum

---
+++ Scrum/2024-07-02/새 폴더/asdva/src/Hello.java
@@ -0,0 +1,22 @@
+public class Hello {
+    public class Main {
+        public static void main(String[] args) {
+            int s =90;
+    
+            switch (s) {
+            case 95:
+                System.out.println("A");
+    
+            case 85:
+                System.out.println("B");
+            case 75:
+                System.out.println("C");
+            case 65:
+                System.out.println("D");
+            default:
+                System.out.println("F");
+            }
+            
+        }
+    }
+}
